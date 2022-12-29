8174480aad86de5c5757d753971041726850ee8c
Michelangelo <mikimarconi2001@gmail.com>
2022-12-29 11:59:47 +0100

    login e sql

--- GestioneFarmacia/main.py
+++ GestioneFarmacia/main.py
@@ -1,14 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5 import uic, QtWidgets
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
 
-from GestioneFarmacia.Grafica.login import Ui_Login
+from Gui.login import Ui_Login
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    Login = QtWidgets.QDialog()
+
+    app = QApplication([])
+    Login = QtWidgets.QWidget()
     ui = Ui_Login()
-    ui.setupUi()
+    ui.setupUi(Login)
     Login.show()
     sys.exit(app.exec_())
