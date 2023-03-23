00d2a573e25618dea5f7843a58132017e7b052d0
Michelangelo <mikimarconi2001@gmail.com>
2023-03-23 18:44:36 +0100

    modifiche varie

--- GestioneFarmacia/GestioneTamponi/Tampone.py
+++
@@ -1,9 +0,0 @@
-from GestioneFarmacia.GestioneVendite.Prodotto import Prodotto
-
-class Tampone(Prodotto):
-
-    def __init__(self, codice, nome, tipologia, prezzo, dosaggio, scadenza, giacenza):
-        super().__init__(codice, nome, tipologia, prezzo, None, scadenza, giacenza)
-        self.esito = False
-
-
